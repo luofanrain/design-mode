a0a3a3da9af101a2ab9130f3fc9bd78f9f1113c0
lixiandong <1779333834@qq.com>
2022-05-23 10:46:01 +0800

    开放封闭原则

---
+++ js/open-or-close-mode/index.ts
@@ -0,0 +1,99 @@
+// 开放封闭原则  原理在于  封闭修改，开放扩展，由于之前写简单工厂模式已包含此特性，我直接copy过来了
+
+class Calculate{
+  @verityNumber
+  calc(){
+    return (this as any).getResult()
+  }
+}
+
+function supplement(){
+  return function(target:any){
+    target.prototype.numberA = 0;
+    target.prototype.numberB = 0;
+  }
+}
+
+function verityNumber(target:any,name:String,descriptor:any){
+  var targetValue = descriptor.value
+  descriptor.value = function(){
+    let reg = /^[\d]+(\.[\d]+)?$/
+    if(!reg.test(this.numberA)) return `numberA必须是数字,错误值：${this.numberA}`
+    if(!reg.test(this.numberB)) return `numberB必须是数字,错误值：${this.numberB}`
+    return targetValue.apply(this,arguments)
+  }
+  return descriptor
+}
+
+
+@supplement()
+class CalcAddition extends Calculate{
+  getResult(){
+    return (this as any).numberA + (this as any).numberB
+  }
+}
+@supplement()
+class CalcSubtraction extends Calculate{
+  getResult(){
+    return (this as any).numberA - (this as any).numberB
+  }
+}
+@supplement()
+class CalcMultiplication extends Calculate{
+  getResult(){
+    return (this as any).numberA * (this as any).numberB
+  }
+}
+@supplement()
+class CalcDivision extends Calculate{
+  getResult(){
+    if((this as any).numberB == 0) return '除数不能为0'
+    return (this as any).numberA / (this as any).numberB
+  }
+}
+@supplement()
+class CalcFactorial extends Calculate{
+  getResult(){
+    if((this as any).numberA == 0) return (this as any).numberA
+    return Math.pow((this as any).numberA,(this as any).numberB)
+  }
+}
+
+class CalcOperator {
+  operator:String = ""
+  createOperator(operator:String){
+    this.operator = operator
+    let exec:Calculate | null = null
+    switch(this.operator){
+      case '+':
+        exec = new CalcAddition()
+        break;
+      case '-':
+        exec = new CalcSubtraction()
+        break;
+      case '*':
+        exec = new CalcMultiplication()
+        break;
+      case '/':
+        exec = new CalcDivision()
+        break;
+      case '**':
+        exec = new CalcFactorial()
+        break;
+      default:
+        console.log('错误的操作符')
+        return null
+    }
+    return exec
+  }
+}
+function main(){
+  let operator:any = new CalcOperator()
+  let item:any = operator.createOperator("**")
+  item.numberA = 10
+  item.numberB = 3
+  console.log(item.calc())
+}
+
+main()
+
